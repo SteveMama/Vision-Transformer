a84ef6469fa3169ff2d695d14cd335130a3b1d48
SteveMama <pkompally@gmail.com>
2024-10-16 17:32:04 -0400

    --Multihead attention added--

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ class  ViT(nn.Module):
 
 
 
+
+
 def patch_embedding(images, n_patches):
 
     n, c, h, w = images.shape # n, 1, 28, 28
@@ -75,4 +77,43 @@ def positional_embedding(sequence_length, d):
 
     return result
 
+
+class MultiHeadAttention(nn.Module):
+    
+    def __init__(self, d, n_heads):
+        super(MultiHeadAttention, self).__init__()
+
+        self.d = d
+        self.n_heads = n_heads
+
+        assert d % n_heads == 0, f'Dimension {d} is not divisible by heads:{n_heads}'
+
+        #patch --> q, k, v w.r.t attn , but not apply across n_heads
+        d_head = int(d / n_heads)
+        self.q = nn.ModuleList([nn.linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.k = nn.ModuleList([nn.linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.v = nn.ModuleList([nn.linear(d_head, d_head) for _ in range(self.n_heads)])
+        self.d_head =  d_head
+        self.softmax = nn.Softmax(dim=-1)
+
+
+    def forward(self, sequences):
+        # N, seq_leng, token_dim
+        result = []
+        for sequence in sequences:
+
+            seq_result = []
+            for head in range(self.n_heads):
+                q_mapping = self.q[head]
+                k_mapping = self.k[head]
+                v_mapping = self.k[head]
+
+                seq = sequence[: head * self.d_head: (head+1) * self.d_head]
+                q, k, v = q_mapping(seq), k_mapping(seq), v_mapping(seq)
+
+                attention = self.softmax(q @ k.T / (self.d_head ** 0.5)) # @ is dot product
+                seq_result.append(attention @ v)
+            result.append(torch.hstack(seq_result))
+
+        return torch.cat([torch.unsqueeze(r, dim=0) for r in result])
 
